657e3b6e44c2281affb317751086bceec892ac08
Dan Leehr <dan.leehr@duke.edu>
2015-11-10 14:01:51 -0500

    Add example script for running native binaries

---
+++ mmap-cwl-example.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+#SBATCH --job-name=MMAP_cwl_example
+#SBATCH --output=MMAP_cwl_example.out
+#SBATCH --mail-user=dan.leehr@duke.edu
+#SBATCH --mail-type=FAIL
+#SBATCH --mem=1024
+
+# All command-line binaries must be in PATH for CWL baseCommand to find them
+BASEPATH="/winhomes/dcl9/mmap-bin/"
+
+## Genovo
+# assemble and finalize
+export PATH="$BASEPATH/genovo-0.4:$PATH"
+
+## Glimmer
+# g3-iterated.csh
+export PATH="$BASEPATH/glimmer3.02/scripts:$PATH"
+# extract
+export PATH="$BASEPATH/glimmer3.02/bin:$PATH"
+# number_orfs.py
+export PATH="$BASEPATH/mmap-bin:$PATH"
+
+## BLAST
+# blastx
+export PATH="$BASEPATH/ncbi-blast-2.2.31+/bin:$PATH"
+# extract_go_terms-cli.py
+export PATH="$BASEPATH/extract-go-terms:$PATH"
+
+# CWL is installed in python virtualenv
+source ~/env-cwl/bin/activate
+
+srun cwltool --preserve-environment PATH \
+  --no-container \
+  --outdir /home/dcl9/mmap-cwl-data/output \
+  /home/dcl9/mmap-cwl/mmap-align.cwl \
+  /home/dcl9/mmap-cwl-data/input/mgm4589537.3.050.json
