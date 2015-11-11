f5872041420f8269efcf7ae2c3a273751a34179f
Dan Leehr <dan.leehr@duke.edu>
2015-11-11 14:27:17 -0500

    Update array job to use fastq pipeline

--- mmap-cwl-array-Sample_LcNi4.sh
+++ mmap-cwl-array-Sample_LcNi4.sh
@@ -10,10 +10,10 @@
 INPUT_DIR=/data/sciencesupport/mmap/Project_MCK_11177_B01_CUS_WGS.2015-10-30/Sample_LcNi4/fastq
 INPUT_FILES=($INPUT_DIR/*.fastq.gz)
 
-FASTA_FILE=${INPUT_FILES[$SLURM_ARRAY_TASK_ID]}
+FASTQ_FILE=${INPUT_FILES[$SLURM_ARRAY_TASK_ID]}
 OUTPUT_BASE=$(basename $FASTA_FILE)
 OUTPUT_CSV_FILE= ${OUTPUT_BASE}.mmap_results.csv
 OUTPUT_DIR=/data/sciencesupport/mmap/results/${OUTPUT_BASE}
 mkdir -p $OUTPUT_DIR
 
-srun mmap-run-single.sh $FASTA_FILE $OUTPUT_DIR
+srun mmap-fastq-run-single.sh $FASTQ_FILE $OUTPUT_DIR
---
+++ mmap-fastq-run-single.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+FASTQ_FILE=$1
+OUTPUT_DIR=$2
+OUTPUT_FILE=$(basename $FASTQ_FILE).mmap_results.csv
+echo "Starting MMAP for $FASTQ_FILE, results in $OUTPUT_FILE"
+
+# All command-line binaries must be in PATH for CWL baseCommand to find them
+BASEPATH="/winhomes/dcl9/mmap-bin/"
+
+## fastx-toolkit
+# fastq_to_fasta
+export PATH="$BASEPATH/fastx_toolkit_0.0.13_binaries_Linux_2.6_amd64/bin:$PATH"
+
+## Genovo
+# assemble and finalize
+export PATH="$BASEPATH/genovo-0.4:$PATH"
+
+## Glimmer
+# g3-iterated.csh
+export PATH="$BASEPATH/glimmer3.02/scripts:$PATH"
+# extract
+export PATH="$BASEPATH/glimmer3.02/bin:$PATH"
+# number_orfs.py
+export PATH="$BASEPATH:$PATH"
+
+## BLAST
+# blastx
+export PATH="$BASEPATH/ncbi-blast-2.2.31+/bin:$PATH"
+# extract_go_terms-cli.py
+export PATH="$BASEPATH/extract-go-terms:$PATH"
+
+# CWL is installed in python virtualenv
+source ~/env-cwl/bin/activate
+
+cwltool --preserve-environment PATH \
+  --no-container \
+  --outdir $OUTPUT_DIR \
+  /home/dcl9/mmap-cwl/mmap-fastq.cwl \
+  --input_fastq_file $FASTQ_FILE \
+  --blast_db_dir /home/dcl9/mmap-cwl-data/input/go-blastdb \
+  --blast_db_name go-seqdb \
+  --output_goterms_file_name $OUTPUT_FILE
+
+
