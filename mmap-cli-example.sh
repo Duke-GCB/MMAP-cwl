627198fa66fcf4a641432610af9b88fe1b73cebf
Dan Leehr <dan.leehr@duke.edu>
2015-11-11 13:40:00 -0500

    add script to run example fasta file through CLI

---
+++ mmap-cli-example.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+#SBATCH --job-name=MMAP_cwl_example
+#SBATCH --output=MMAP_cwl_example.out
+#SBATCH --mail-user=dan.leehr@duke.edu
+#SBATCH --mail-type=FAIL
+#SBATCH --mem=1024
+
+INPUT_DIR=/home/dcl9/mmap-cwl-data/example/input
+INPUT_FILES=($INPUT_DIR/*.fasta)
+
+FASTA_FILE=${INPUT_FILES[0]}
+OUTPUT_BASE=$(basename $FASTA_FILE)
+OUTPUT_CSV_FILE= ${OUTPUT_BASE}.mmap_results.csv
+OUTPUT_DIR=/home/dcl9/mmap-cwl-data/example/output/${OUTPUT_BASE}
+mkdir -p $OUTPUT_DIR
+
+srun mmap-run-single.sh $FASTA_FILE $OUTPUT_DIR
